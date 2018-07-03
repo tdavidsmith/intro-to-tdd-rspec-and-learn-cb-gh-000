9eb9ddf80d031883122ebb39bc9fe1c8c43f82a1
Timothy Smith <tdavidsmith.1234@gmail.com>
2018-07-03 03:09:20 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -2,4 +2,4 @@
 def current_age_for_birth_year(birth_year)
   2004 - birth_year
 end
-```
+https://learn.co/tracks/full-stack-community-bootcamp/intro-to-ruby-development/methods/tdd-rspec-and-learn#```
